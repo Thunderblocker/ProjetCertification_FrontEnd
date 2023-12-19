13d93bfd5e62c63e3fd468e382340e304c37bc23
AlexandreBouirik <98479232+Mysterio228@users.noreply.github.com>
2023-12-19 15:12:39 +0100

    FetcherService Generic

--- src/app/services/fetcher.service.ts
+++ src/app/services/fetcher.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetcherService {
+  private apiUrl = 'http://localhost:8080/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll<T>(item: string): Observable<T[]>{
+    return this.http.get<T[]>(`${this.apiUrl}/${item}`);
+  }  
+  
+  getById<T>(item: string, id: number): Observable<T>{
+    return this.http.get<T>(`${this.apiUrl}/${item}/${id}`);
+  }
 }
