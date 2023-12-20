ce794990a26b59c3463a9c0ac72d9fe22be9d3b7
sofiane <idri.sof3@gmail.com>
2023-12-20 10:37:52 -0500

    add component users + service users+ changement channels

--- src/app/component/messages/messages.component.ts
+++ src/app/component/messages/messages.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {MessagesService} from "../../service/messages.service";
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+    imports: [
+        NgForOf
+    ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
 export class MessagesComponent {
+  constructor(public messagesService: MessagesService) {
 
+  }
 }
---
+++ src/app/model/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+     id: number;
+     pseudo: string;
+     prenom:string;
+     nom:string;
+     image:string;
+
+}
